17e362211c00da5f3b404db44e45cb5209614c6c
Brennon Schow <me@brennongs.dev>
2022-06-07 22:50:36 -0600

    add observable

---
+++ adapters/observable/index.ts
@@ -0,0 +1,48 @@
+import Contract, {
+  Observer,
+  Subscriber
+} from './.d';
+
+export class Observable<T> implements Contract<T> {
+  observers: Array<Observer<T>> = []
+  state: T;
+
+  constructor(state: T) {
+    console.log(state)
+    this.state = { ...state }
+  }
+
+  emit(action: (state: T) => T) {
+    if (action) {
+      const result = action(this.state)
+      console.log(result)
+      if (result) this.state = {...result}
+    }
+
+    this.observers.forEach(observer => {
+      observer.handle(this.state)
+    })
+
+    return this;
+  };
+
+  subscribe(handle: (state: T) => any): Subscriber {
+    const observer: Observer<T> = {
+      id: Symbol('id'),
+      handle
+    }
+
+    this.observers.push(observer)
+    observer.handle(this.state)
+
+    return {
+      unsubscribe: () => {
+        this.unsubscribe(observer.id)
+      }
+    }
+  }
+
+  unsubscribe(id: Symbol): void {
+    this.observers = this.observers.filter(observer => observer.id !== id)
+  };
+}
